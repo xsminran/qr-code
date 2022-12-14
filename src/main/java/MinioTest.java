e5be13ffcbff168a90ae401e94ee48ab7030187c
minran <2841310249@qq.com>
2022-12-14 17:31:52 +0800

    minio测试

---
+++ src/main/java/MinioTest.java
@@ -0,0 +1,45 @@
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
+import io.minio.errors.MinioException;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class MinioTest {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+        try {
+            // Create a minioClient with the MinIO server playground, its access key and secret key.
+            MinioClient minioClient =
+                    MinioClient.builder()
+                            .endpoint("http://124.70.90.123:9000")
+                            .credentials("MLY528G8R8UJI7X7283N", "N+QWB6DboeKPMVopYlS0WfCQojDL18Xk3mpOb4nu")
+                            .build();
+
+            // Make 'asiatrip' bucket if not exist.
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket("public").build());
+            if (!found) {
+                // Make a new bucket called 'asiatrip'.
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket("public").build());
+            } else {
+                System.out.println("Bucket 'public' already exists.");
+            }
+
+            // Upload '/home/user/Photos/asiaphotos.zip' as object name 'asiaphotos-2015.zip' to bucket
+            // 'asiatrip'.
+            minioClient.uploadObject(
+                    UploadObjectArgs.builder()
+                            .bucket("public")
+                            .object("credentials.json")
+                            .filename("C:/Users/minran/Downloads/credentials.json")
+                            .build());
+            System.out.println("'C:/Users/lai.huanxiong/Downloads/credentials.json' is successfully uploaded as " + "object 'credentials.json' to bucket 'public'.");
+        } catch (MinioException e) {
+            System.out.println("Error occurred: " + e);
+            System.out.println("HTTP trace: " + e.httpTrace());
+        }
+    }
+}
